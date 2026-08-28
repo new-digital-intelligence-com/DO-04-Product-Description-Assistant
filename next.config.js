be96f44ea55b80cd7c264fed475cf7285a9887c9
HelmiDev03 <helmipaty@gmail.com>
2026-08-28 18:31:34 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -1,2 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = { outputFileTracingIncludes: { '/api/**': ['./skill/**/*', './data/**/*'] } };
+module.exports = { outputFileTracingIncludes: { '/api/**': ['./plugins/**/*', './data/**/*'] } };
